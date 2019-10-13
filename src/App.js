62895bba9db4c1ceb7201bfbb930ee1cf235eccd
fa6oomawad <fatimamuk0@gmail.com>
2019-10-12 21:15:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./scss/main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="full-page">
+      <div className="side-nav">
+        <h3>Discover</h3>
+        <ul>
+          <li>Top songs</li>
+          <li>Trending Artists</li>
+          <li>New Songs</li>
+          <li>Best of</li>
+        </ul>
+      </div>
     </div>
   );
 }
