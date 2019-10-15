f14d60165dd21eeb770c3f13a334fd432907473e
fa6oomawad <fatimamuk0@gmail.com>
2019-10-14 22:38:55 -0400

    new layout for albums added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,114 @@ function App() {
     <div className="full-page">
       <div className="side-nav">
         <h3>Discover</h3>
-        <ul>
+        <ul className="side-list">
           <li>Top songs</li>
           <li>Trending Artists</li>
           <li>New Songs</li>
           <li>Best of</li>
         </ul>
+      </div>
+      <div className="boxes-wrapper">
+        <div className="grid-wrap">
+          <div className="small-box">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+          <div className="small-box">
+            {" "}
+            <img
+              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+              alt="album cover"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
