7979d1b551ceadfb9cf25bf773b473f9c5edee70
fa6oomawad <fatimamuk0@gmail.com>
2019-10-15 20:57:12 -0400

    elements replaced with compnents

--- src/App.js
+++ src/App.js
@@ -1,121 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./scss/main.css";
-
+import SideNav from './SideNav';
+import MainBox from './MainBox'
 function App() {
   return (
     <div className="full-page">
-      <div className="side-nav">
-        <h3>Discover</h3>
-        <ul className="side-list">
-          <li>Top songs</li>
-          <li>Trending Artists</li>
-          <li>New Songs</li>
-          <li>Best of</li>
-        </ul>
-      </div>
-      <div className="boxes-wrapper">
-        <div className="grid-wrap">
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-          </div>
-        </div>
-      </div>
+    <SideNav/>
+    <MainBox/>
     </div>
   );
 }
