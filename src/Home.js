8fb33365cb8b7100c8af964798fcd4d57e948a23
fa6oomawad <fatimamuk0@gmail.com>
2019-10-23 22:24:43 -0400

    layout edited

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,32 @@ function Home(props) {
   return (
     <div className="homeBox">
       <div className="grid-wrap2">
-        <div className="small-box">
+        <div className="gridbox box1">
           <img
             src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
             alt="album cover"
           />
         </div>
-        <div className="small-box">
+        <div className="gridbox box2">
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+            alt="album cover"
+          />
+        </div>
+
+        <div className="gridbox box3">
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+            alt="album cover"
+          />
+        </div>
+        <div className="gridbox box4">
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+            alt="album cover"
+          />
+        </div>
+        <div className="gridbox box5">
           <img
             src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
             alt="album cover"
