d43c5e6b00bf636e884dbe35e89d4c07d183516c
fa6oomawad <fatimamuk0@gmail.com>
2019-10-23 21:28:02 -0400

    grid for home component added

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div className="homeBox">
+      <div className="grid-wrap2">
+        <div className="small-box">
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+            alt="album cover"
+          />
+        </div>
+        <div className="small-box">
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
+            alt="album cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
