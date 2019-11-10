fc03744908d546f802253417e20e688d81901c24
fa6oomawad <fatimamuk0@gmail.com>
2019-11-09 22:27:22 -0500

    main changed to accept data from api

--- src/MainBox.js
+++ src/MainBox.js
@@ -1,140 +1,20 @@
-import React from 'react';
+import React from "react";
 
-function MainBox (props){
-    return (
-          <div className="boxes-wrapper">
-        <div className="grid-wrap">
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-            <span className="title">song name</span>
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
-          </div>
-          <div className="small-box">
-            {" "}
-            <img
-              src="https://images-na.ssl-images-amazon.com/images/I/91nND-Wro2L._SL1000_.jpg"
-              alt="album cover"
-            />
-                        <span className="title">song name</span>
-
+function MainBox(props) {
+  return (
+    <div className="boxes-wrapper">
+      <div className="grid-wrap">
+        {props.data.slice(0, 15).map(item => {
+          return (
+            <div className="small-box">
+              <img src={item.images[0].url} alt="album cover" />
+              <span className="title">{item.name}</span>
             </div>
-            
-        </div>
+          );
+        })}
       </div>
-    )
+    </div>
+  );
 }
 
 export default MainBox;
