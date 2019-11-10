0ea23a25aec6029b5d6ecc9556edcb509f99ed94
fa6oomawad <fatimamuk0@gmail.com>
2019-11-09 23:14:21 -0500

    changed into class

--- src/MainBox.js
+++ src/MainBox.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
-function MainBox(props) {
-  return (
-    <div className="boxes-wrapper">
-      <div className="grid-wrap">
-        {props.data.slice(0, 15).map(item => {
-          return (
-            <div className="small-box">
-              <img src={item.images[0].url} alt="album cover" />
-              <span className="title">{item.name}</span>
-            </div>
-          );
-        })}
+class MainBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playstatus: false,
+      songlink: null
+    };
+    this.settingPlayer = this.settingPlayer.bind(this);
+  }
+  settingPlayer(link) {
+    this.setState({ playstatus: true, songlink: link });
+  }
+  render() {
+    return (
+      <div className="boxes-wrapper">
+        <div className="grid-wrap">
+          {this.props.data.slice(0, 15).map(item => {
+            return (
+              <div className="small-box">
+                <div className="overlay">
+                  <p
+                    className="play"
+                    onClick={() => {
+                      this.settingPlayer();
+                    }}
+                  >
+                    click here
+                  </p>
+                </div>
+                <img src={item.images[0].url} alt="album cover" />
+                <span className="title">{item.name}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MainBox;
