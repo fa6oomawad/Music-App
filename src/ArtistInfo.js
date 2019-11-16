c4edac6070f3e0ca022228f82c5df5aae496740e
fa6oomawad <fatimamuk0@gmail.com>
2019-11-16 16:52:44 -0500

    grid added to artist comp

--- src/ArtistInfo.js
+++ src/ArtistInfo.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
-function ArtistInfo (porps){
-    return (
-        <div>
-            artist information
-            </div>
-    )
+function ArtistInfo(props) {
+  return (
+    <div className="boxes-wrapper">
+      <div className="grid-wrap22">
+        <div className="wrap2 box11">
+          <h1>Singer name</h1>
+        </div>
+        <div className="wrap2 box22">
+          <img src="" alt="signer img" />
+        </div>
+      </div>
+    </div>
+  );
 }
+export default ArtistInfo;
